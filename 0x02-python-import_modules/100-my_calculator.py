5fcb18e432e1eac4ad7a9de30237e0fd8765d407
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-08 23:12:48 +0100

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,23 +2,27 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count != 3:
+
+    count = len(sys.argv)
+    if count != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in operators:
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+
+    if operator not in "+-/*":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+    if operator == '+':
+        result = add(a, b)
+    if operator == '-':
+        result = sub(a, b)
+    if operator == '*':
+        result = mul(a, b)
+    if operator == '/':
+        result = div(a, b)
+
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
