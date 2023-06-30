ab6b7c3528fd072e6f544cc8a06f1301f8bced95
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-30 21:23:38 +0100

    3 10 7

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def get_last_digit(number):
-    last_digit = abs(number) % 10
-    return str(last_digit)
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
