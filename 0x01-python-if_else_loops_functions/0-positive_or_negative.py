eb9fcab58b5015523917aa7b0a8577f89301f48b
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-30 17:35:14 +0100

    Positive anything is better than negative nothing

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,17 +2,13 @@
 
 import random
 
-number = random.randint(-10000, 10000)
+number = random.randint(-10, 10)
 
-print("Last digit of", number, "is", abs(number) % 10, end=" ")
+print(number, end=" ")
 
-last_digit = abs(number) % 10
-
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
+if number > 0:
+    print("is positive")
+elif number < 0:
+    print("is negative")
 else:
-    print("and is less than 6 and not 0")
-
-print()  # Print a new line
+    print("is zero")
