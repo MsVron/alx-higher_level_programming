879b59284759e4b13754a5de87b666f440a39f84
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-30 20:45:46 +0100

    The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-for i in range(10):
+output = []
+
+for i in range(0, 10):
     for j in range(i+1, 10):
-        print("{:02d}, {:02d}".format(i, j), end=", ")
-print("89")
+        output.append("{:02d}".format(i) + "{:02d}".format(j))
+
+output_str = ", ".join(output)
+print(output_str)
