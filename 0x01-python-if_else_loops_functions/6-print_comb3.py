240e3560f89703ea39bcd3931377d11a2d1eb58b
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-30 20:39:51 +0100

    The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(1, 10):
-    for j in range(i + 1, 10):
-        if i == 9 and j == 9:
-            print("{}{}".format(i, j), end="")
-        else:
-            print("{}{}, ".format(i, j), end="")
+for i in range(10):
+    for j in range(i+1, 10):
+        print("{:02d}".format(i), end=", ")
+        print("{:02d}".format(j), end=", ")
+print("89")
