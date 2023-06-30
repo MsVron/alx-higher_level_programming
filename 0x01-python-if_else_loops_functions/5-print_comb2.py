9efd7e75d265a20764e6151ddea3d55e51e058e9
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-30 20:19:56 +0100

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-for num in range(100):
-    print("{:02d}".format(num), end=', ' if num != 99 else '')
+for num in range(99):
+    print("{:02d}, ".format(num), end='')
+
+print("{:02d}".format(99))
