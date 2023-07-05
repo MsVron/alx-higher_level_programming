87bdba408e6ad8e5dbf7d456592ea5c2b91a745c
Aya Berroukech <ayaberrou01@gmail.com>
2023-07-05 22:39:47 +0000

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    This class provides a basic representation of a rectangle with no specific properties or behavior defined.
+    """
+
+    pass
+
