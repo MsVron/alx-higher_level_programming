31b7fe40cbe0e9574a5426c531bf07824c36fee3
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-06 01:57:46 +0100

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+Module class LockedClass
+"""
 class LockedClass:
+    """
+    Class that's locked unless you're mister or madam first_name
+    """
     __slots__ = ["first_name"]
