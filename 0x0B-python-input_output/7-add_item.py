161ceb3a90f761730742821262e0053cfb863319
Aya Berroukech <ayaberrou01@gmail.com>
2023-07-11 12:21:30 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
 
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """
-Creates an object from a JSON file.
+Adds all arguments to a Python list
+and saves them to a file using JSON representation.
 """
-import json
 
 
-def load_from_json_file(filename):
+def main():
     """
-    :param filename: The name of the JSON file to load the object from.
-    :type filename: str
-    :return: The object created from the JSON file.
-    :rtype: object
+    The list is saved as a JSON representation
+    in a file named "add_item.json".
+    If the file doesn’t exist, it will be created.
     """
-    with open(filename, "r") as file:
-        return json.load(file)
+
+    filename = "add_item.json"
+
+    try:
+        # Load existing data from the file
+        data = load_from_json_file(filename)
+    except FileNotFoundError:
+        # If the file doesn't exist, start with an empty list
+        data = []
+
+    # Add all arguments to the list
+    data.extend(sys.argv[1:])
+
+    # Save the updated list to the file
+    save_to_json_file(data, filename)
+
+if __name__ == '__main__':
+    main()
