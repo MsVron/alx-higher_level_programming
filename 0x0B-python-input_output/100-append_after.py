d3c2ed954b25301ee57e0d1be0bca4a01c08b4a5
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-12 01:01:08 +0100

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -21,28 +21,29 @@ def append_after(filename="", search_string="", new_string=""):
         None
     """
     tmp_fn = 'temp.txt'  # Name of the temporary file
-    
+
     # Open the input file in read mode and create a temporary output file
     with open(filename, 'r') as input_file, open(tmp_fn, 'w') as output_file:
         # Iterate through each line in the input file
         for line in input_file:
             # Write the current line to the output file
             output_file.write(line)
-            
+
             # Check if the search string is present in the current line
             if search_string in line:
                 # Write the new string after the line
                 # containing the search string
                 output_file.write(new_string + '\n')
-    
+
     # Replace the input file with the temporary output file
     with open(tmp_fn, 'r') as temp_file, open(filename, 'w') as input_file:
         # Copy the contents of the temporary file back to the input file
         for line in temp_file:
             input_file.write(line)
-    
+
     # Remove the temporary file
     remove_file(tmp_fn)
+
 
 """
 Removes a file.
