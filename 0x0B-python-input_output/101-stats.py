52bcdf3061792c737c0ed176e99ab84a2e389ce8
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-12 11:50:45 +0100

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -21,8 +21,7 @@ def print_statistics(total_file_size, status_code_counts):
     """
     Args:
         total_file_size (int): The total file size.
-        status_code_counts (dict): A dictionary
-        containing counts of status codes.
+        status_code_counts (dict): A dictionary containing counts of status codes.
 
     Raises:
         None
@@ -30,7 +29,7 @@ def print_statistics(total_file_size, status_code_counts):
     Returns:
         None
     """
-    print("Total file size:", total_file_size)
+    print("File size:", total_file_size)
     for status_code in sorted(status_code_counts.keys()):
         count = status_code_counts[status_code]
         print(status_code, ":", count)
@@ -59,13 +58,13 @@ def process_input():
         # Read input line by line from stdin
         for line in sys.stdin:
             # Extract file size from the line using regular expression
-            match = re.match(r".*HTTP/1\.1\" \d+ (\d+)$", line)
+            match = re.match(r".*\s(\d+)$", line)
             if match:
                 file_size = int(match.group(1))
                 total_file_size += file_size
 
             # Extract status code from the line using regular expression
-            match = re.match(r".*HTTP/1\.1\" (\d+)", line)
+            match = re.match(r".*\".*?\" (\d+)", line)
             if match:
                 status_code = match.group(1)
                 if status_code in status_code_counts:
