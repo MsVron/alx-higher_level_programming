1e19d5c9268ec8b0a092141a2c670d04ea3a27f4
Aya Berroukech <ayaberrou01@gmail.com>
2023-07-13 00:19:35 +0000

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+A class representing a rectangle.
+"""
+
+
+class Rectangle(BaseGeometry):
+
+    """
+    Initializes a Rectangle instance with the given width and height.
+    """
+
+    def __init__(self, width, height):
+        """
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
+        """
+        self.__width = self.__height = None
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    """
+    Computes and returns the area of the rectangle.
+    """
+
+    def area(self):
+        """
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    """
+    Returns a string representation of the rectangle.
+    """
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
