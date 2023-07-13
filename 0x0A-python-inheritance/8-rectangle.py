61453419c5b9b9306988b408656652294b964ab1
Aya Berroukech <ayaberrou01@gmail.com>
2023-07-13 00:21:32 +0000

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+A class representing a rectangle.
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Initializes a Rectangle instance with the given width and height.
+    """
+
+    def __init__(self, width, height):
+        """
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
+        """
+        self.__width = None
+        self.__height = None
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
