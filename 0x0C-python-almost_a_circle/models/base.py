4e8a854398bc783113df2f8f7f97b5eb0d447914
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-17 23:41:06 +0100

    First Rectangle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,27 +1,86 @@
 #!/usr/bin/python3
 
-"""Base class for managing id attribute in all future classes."""
+"""Class representing a Rectangle."""
 
 
-class Base:
+from models.base import Base
 
+class Rectangle(Base):
     """
-    Initialize a Base instance.
+    Rectangle class that inherits from Base.
+    
+    Attributes:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+        x (int, optional): x coordinate of the rectangle. Defaults to 0.
+        y (int, optional): y coordinate of the rectangle. Defaults to 0.
+        id (int, optional): id of the rectangle. If not provided, an id is 
+            automatically assigned using the logic from the Base class.
     """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
+    
+    """Initialize a new Rectangle instance."""
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
         """
         Args:
-            id (int, optional): The id of the instance.
-                If provided, the id attribute will be set to this value.
-                If not provided, the id attribute will be incremented using
-                the __nb_objects attribute.
-
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+            x (int, optional): x coordinate of the rectangle. Defaults to 0.
+            y (int, optional): y coordinate of the rectangle. Defaults to 0.
+            id (int, optional): id of the rectangle. If not provided, an id is 
+                automatically assigned using the logic from the Base class.
         """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    """Get the width of the rectangle."""
+
+    @property
+    def width(self):
+        return self.__width
+
+    """Set the width of the rectangle."""
+
+    @width.setter
+    def width(self, width):
+
+        self.__width = width
+
+    """Get the height of the rectangle."""
+
+    @property
+    def height(self):
+        return self.__height
+
+    """Set the height of the rectangle."""
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+        """Get the x coordinate of the rectangle."""
+        
+    @property
+    def x(self):
+        return self.__x
+
+    """Set the x coordinate of the rectangle."""
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    """Get the y coordinate of the rectangle."""
+
+    @property
+    def y(self):
+        return self.__y
+
+    """Set the y coordinate of the rectangle."""
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
