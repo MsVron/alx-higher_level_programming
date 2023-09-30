612109f6ab9fa419084018a87181037fe0bbd807
Aya <126300537+MsVron@users.noreply.github.com>
2023-09-30 23:57:43 +0100

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me to trigger the desired response
-curl -sX PUT -d "user_id=98" -H "Origin: HolbertonSchool" "$1"
+curl -sL -X PUT 0.0.0.0:5000/catch_me_3
