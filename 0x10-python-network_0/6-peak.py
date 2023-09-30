f96937b2ebae2c3f6a1265a210bfcb1658b663db
Aya Berroukech <ayagym14@gmail.com>
2023-09-30 15:37:10 -0700

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Find peak
+"""
+
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): List of unsorted integers.
+
+    Returns:
+        int: A peak element in the list, or None if the list is empty.
+
+    Complexity:
+        The complexity of this algorithm is O(log(n)).
+    """
+
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
