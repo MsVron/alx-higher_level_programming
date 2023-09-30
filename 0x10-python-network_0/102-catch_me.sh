e4096207ba0731a8394d4a3fd3f3d6025a957173
Aya <126300537+MsVron@users.noreply.github.com>
2023-09-30 23:53:17 +0100

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a PUT request to the specific URL to trigger the desired response
-curl -sX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me_2" -L
-
+curl -sX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
