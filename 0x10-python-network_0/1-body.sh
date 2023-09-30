37bb0f0bc102f479a4e39eb198062f531ce77619
Aya <126300537+MsVron@users.noreply.github.com>
2023-10-01 00:04:29 +0100

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request and displays the body of a 200 status code response
-curl -s -o /dev/null --write-out "%{http_code}" "$1" | [ "$(cat)" == "200" ] && curl -s "$1"
+curl -sX GET $1 -L
